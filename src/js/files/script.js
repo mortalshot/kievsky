8b3535f2eeac633df31366c14653479440da97f3
mortalshot <seregagl1996@gmail.com>
2023-09-15 15:15:09 +0300

    v1.0

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,5 +1,5 @@
 // Подключение функционала 
-import { isMobile } from "./functions.js";
+import { isMobile, removeClasses } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
 
@@ -114,6 +114,201 @@ if (videos.length > 0) {
     })
   });
 }
+
+// Фильтрация магазинов по названию
+const template6Search = document.querySelector('.template6__search input');
+const template6Items = document.querySelectorAll('.template6-item__name');
+
+if (template6Search && template6Items.length > 0) {
+  template6Search.addEventListener('input', function () {
+    var query = this.value.toLowerCase();
+
+    for (var i = 0; i < template6Items.length; i++) {
+      var text = template6Items[i].textContent.toLowerCase();
+      let itemParent = template6Items[i].closest('.template6-item');
+
+      if (text.indexOf(query) !== -1) {
+        // Если элемент содержит введенный текст, то он отображается
+        itemParent.style.display = 'block';
+      } else {
+        // Если элемент не содержит введенный текст, то он скрывается
+        itemParent.style.display = 'none';
+      }
+    }
+  })
+}
+
+// Схема комплекса
+const tradeArea = document.querySelectorAll('.trade-area');
+if (tradeArea.length > 0) {
+  tradeArea.forEach(area => {
+    // Подсвечиваем площадь при наведении
+    area.addEventListener('mouseover', function () {
+      const areaID = area.dataset.id;
+      const selectItem = document.querySelectorAll(`[data-id="${areaID}"]`);
+
+      removeClasses(tradeArea, '_hover');
+      selectItem.forEach(element => {
+        element.classList.add('_hover');
+      });
+    })
+
+    area.addEventListener('mouseout', function () {
+      removeClasses(tradeArea, '_hover');
+    })
+
+    // Наполняем контентом попап магазина
+    area.addEventListener('click', function () {
+      const popupMain = document.querySelector('.store .popup__text');
+      const areaID = area.dataset.id;
+      const infoStore = document.querySelector(`.info-store[data-id=${areaID}]`);
+      const storePreview = infoStore.querySelector('.info-store__preview');
+      const storeLogo = infoStore.querySelector('.info-store__logo');
+      const storeSchedule = infoStore.querySelector('.info-store__schedule');
+      const storeFloor = infoStore.querySelector('.info-store__floor');
+      const storeTitle = infoStore.querySelector('.info-store__title');
+      const storeCategory = infoStore.querySelector('.info-store__category');
+      const storeText = infoStore.querySelector('.info-store__text');
+      const storeLink = infoStore.querySelector('.info-store__link');
+      const storeGallery = infoStore.querySelector('.store__gallery-list');
+
+      console.log(storeGallery);
+
+      // Добавляем фоновую картинку
+      if (storePreview.textContent) {
+        popupMain.querySelector('.store__preview').classList.add('_filled');
+
+        popupMain.querySelector('.store__preview').innerHTML = `
+          <img src="${storePreview.textContent}" alt="">
+
+          <button data-close type="button" class="popup__close _icon-close"></button>
+        `;
+      }
+
+      // Добавляем логотип компании
+      if (storeLogo.textContent) {
+        popupMain.querySelector('.store__logo').innerHTML = `
+          <img src="${storeLogo.textContent}" alt="">
+        `;
+      }
+
+      // Добавляем расписание компании
+      if (storeSchedule.textContent) {
+        popupMain.querySelector('.store-schedule__body').innerHTML = storeSchedule.outerHTML;
+      }
+
+      // Добавляем этаж, на котором находится магазин
+      if (storeFloor.textContent) {
+        popupMain.querySelector('.store__location-floor span').innerHTML = storeFloor.textContent;
+      }
+
+      // Добавляем название компании
+      if (storeTitle.textContent) {
+        popupMain.querySelector('.store__title').innerHTML = storeTitle.textContent;
+      }
+
+      // Добавляем название компании
+      if (storeCategory.textContent) {
+        popupMain.querySelector('.store__category').innerHTML = storeCategory.textContent;
+      }
+
+      // Добавляем описание компании
+      if (storeText.textContent) {
+        popupMain.querySelector('.store__text').innerHTML = storeText.textContent;
+      }
+
+      // Добавляем ссылку на магазин
+      if (storeLink.textContent) {
+        popupMain.querySelector('.store__link').innerHTML = `
+          <a href="${storeLink.textContent}" target="_blank">
+            <i class="link__arrow"></i>
+            <span class="link__text">Подробнее</span>
+          </a>
+        `;
+      }
+
+      // Добавляем галерею компании
+      if (storeGallery.textContent) {
+        popupMain.querySelector('.store__gallery').innerHTML = storeGallery.outerHTML;
+      }
+    })
+  });
+}
+
+// Поиск магазина на схеме комплекса
+const schemeSearch = document.querySelector('.scheme-search__input');
+const schemeStores = document.querySelectorAll('.scheme-search__store');
+
+if (schemeSearch) {
+  schemeSearch.addEventListener('input', function () {
+    var query = this.value.toLowerCase();
+
+    for (var i = 0; i < schemeStores.length; i++) {
+      var text = schemeStores[i].textContent.toLowerCase();
+      if (text.indexOf(query) !== -1) {
+        // Если элемент содержит введенный текст, то он отображается
+        schemeStores[i].style.display = 'flex';
+      } else {
+        // Если элемент не содержит введенный текст, то он скрывается
+        schemeStores[i].style.display = 'none';
+      }
+    }
+  })
+}
+
+// Код работает только на странице Схема комплекса
+if (document.body.classList.contains('page-template-scheme')) {
+  const url = window.location.href;
+  const parts = url.split('?');
+
+  // Выделение цветом нужного магазина после перехода по ссылке "Показать на карте"
+  if (parts.length > 1) {
+    const paramIndex = parts[1].indexOf('dataId=');
+    const paramValue = parts[1].substring(paramIndex + 7);
+
+    const selectItem = document.querySelectorAll(`[data-id="${paramValue}"]`);
+    selectItem.forEach(element => {
+      element.classList.add('_hover');
+    });
+
+    // Очищаем строку браузера
+    const clearedUrl = url.split('?')[0];
+    window.history.pushState({}, '', clearedUrl);
+  }
+}
+
+
+document.addEventListener("afterPopupClose", function (e) {
+  const currentPopup = e.detail.popup;
+
+  // Обнуляем попап компании
+  if (currentPopup.hash == "#store-info") {
+    const popupPreview = document.querySelector('.store .store__preview');
+    const popupLogo = document.querySelector('.store .store__logo');
+    const popupText = document.querySelector('.store .store__text');
+    const popupLink = document.querySelector('.store .store__link');
+    const popupGallery = document.querySelector('.store .store__gallery');
+
+    // Обнуляем превью попапа
+    popupPreview.classList.remove('_filled');
+    popupPreview.innerHTML = `
+      <button data-close type="button" class="popup__close _icon-close"></button>
+    `;
+
+    // Обнуляем лого компании
+    popupLogo.innerHTML = " ";
+
+    // Обнуляем описание компании
+    popupText.innerHTML = " ";
+
+    // Обнуляем ссылку на компанию
+    popupLink.innerHTML = " ";
+
+    // Обнуляем галерею компании
+    popupGallery.innerHTML = " ";
+  }
+});
+
 
 
 window.addEventListener('DOMContentLoaded', gsapAnimations);
